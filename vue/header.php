39f2f5f0d8d0e895ee514048f6f991ff13dfb0a4
Lucien Zak <lucienzak@MacBook-Pro-de-Lucien.local>
2022-01-07 09:19:23 +0100

    correction de push ;)

---
+++ vue/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../vue/CSS/styles.css">
+    <title>Memory</title>
+</head>
+<header>
+    <nav id="navbar">
+        <ul>
+            <li id="logo"><img class="logo" src="../assets/images/logo.png" alt="Ampoule et cerveau"></li>
+            <li class="lien">Acceuil</li>
+            <li class="lien">Mon Profil</li>
+            <li class="lien">Tableau des scores</li>
+        </ul>
+    </nav>
+</header>
